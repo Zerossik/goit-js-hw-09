a03956193c6c40edaeecb5082aec59a170029afe
Zerossik <zeross19940303@gmail.com>
2023-03-21 00:46:14 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
 let selectDate = null;
 const currentData = new Date().getTime();
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -10,9 +12,10 @@ const options = {
   onClose(selectedDates) {
     selectDate = selectedDates[0].getTime();
     checkValidDate();
-    startTimer();
+    const time = convertMs(selectDate - currentData);
   },
 };
+
 const refs = {
   inputEl: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
@@ -27,9 +30,22 @@ function checkValidDate() {
     alert('Please choose a date in the future');
   }
 }
-function startTimer() {
-  setInterval(() => {
-    selectDate -= 1000;
-    console.log(selectDate);
-  }, 1000);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
