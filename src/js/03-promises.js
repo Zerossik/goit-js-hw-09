188a70a578590c8871767ef52e66e5df14be2460
Zerossik <zeross19940303@gmail.com>
2023-03-22 20:49:21 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
+const delay = refs.formEl.delay.value;
+const step = refs.formEl.step.value;
+const amount = refs.formEl.amount.value;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
+createPromise(2, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
