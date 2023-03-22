942e0abf121729596c3f317a409f475f0f764968
Zerossik <zeross19940303@gmail.com>
2023-03-23 01:24:48 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,11 @@
 const refs = {
   formEl: document.querySelector('.form'),
 };
+let delay = null;
+let step = null; // тут хранятся значения полей формы.
+let amount = null;
 
-const delay = refs.formEl.delay.value;
-const step = refs.formEl.step.value;
-const amount = refs.formEl.amount.value;
-
+// Функция, которая создает 1 промис...
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -16,10 +16,33 @@ function createPromise(position, delay) {
     }
   });
 }
-createPromise(2, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+
+// Алгоритм
+// 1- создать функцию обработчик по submit
+// 2- цикл, в котором будет столько итераций, сколько пользователь ввел число в поле Amount.
+// 3- внутри цикла, создать сет интервал, с задержской "delay"/ внутри него вызвать createPromise(i, delay)
+
+let intervalId = null;
+// Функция, которая обрабатывает отправку форм!
+function handlerSubmit(evt) {
+  evt.preventDefault();
+  delay = Number(refs.formEl.delay.value);
+  step = Number(refs.formEl.step.value);
+  amount = Number(refs.formEl.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(delay);
+    intervalId = setTimeout(() => {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, delay);
+    delay += step;
+  }
+}
+
+refs.formEl.addEventListener('submit', handlerSubmit);
