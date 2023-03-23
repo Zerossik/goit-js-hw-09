f8cce4af2dbd877b45b64f19bf72dcbd30c2084d
Zerossik <zeross19940303@gmail.com>
2023-03-23 19:23:26 +0200

    task-3-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,13 @@
 const refs = {
   formEl: document.querySelector('.form'),
 };
-let delay = null;
+let delayMs = null;
 let step = null; // тут хранятся значения полей формы.
 let amount = null;
 
 // Функция, которая создает 1 промис...
 function createPromise(position, delay) {
+  console.log(delay);
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     if (shouldResolve) {
@@ -26,22 +27,20 @@ let intervalId = null;
 // Функция, которая обрабатывает отправку форм!
 function handlerSubmit(evt) {
   evt.preventDefault();
-  delay = Number(refs.formEl.delay.value);
+  delayMs = Number(refs.formEl.delay.value);
   step = Number(refs.formEl.step.value);
   amount = Number(refs.formEl.amount.value);
 
   for (let i = 1; i <= amount; i += 1) {
-    console.log(delay);
     intervalId = setTimeout(() => {
-      createPromise(i, delay)
+      createPromise(i, delayMs)
         .then(({ position, delay }) => {
           console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
         .catch(({ position, delay }) => {
           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
         });
-    }, delay);
-    delay += step;
+    }, (delayMs += step));
   }
 }
 
